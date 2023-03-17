2e6a78f4786577d0476cf52bb4e6e458611ea034
hiensenn <hiensen.cdl@gmail.com>
2023-03-17 02:27:19 -0300

    criando rotas na Controllers e  importando o arquivo de rota em Routes toughts/home

---
+++ controllers/ToughtController.js
@@ -0,0 +1,15 @@
+const Tought = require('../models/Tought')
+const User = require('../models/User')
+
+module.exports = class ToughtController{
+
+
+
+
+    
+
+
+    static async showToughts(req, res){
+        res.render('toughts/home')
+    }
+}
--- index.js
+++ index.js
@@ -13,6 +13,13 @@ const conn = require('./db/conn')
 const Tought = require('./models/Tought')
 const User = require('./models/User')
 
+
+//Import Routes
+const toughtsRoutes = require('./routes/toughtsRoutes')
+
+//Import controller
+const ToughtController = require('./controllers/ToughtController')
+
 //template engine
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
@@ -66,8 +73,12 @@ app.use((req, res, next) => {
 })
 
 
+//Routes
+app.use('/toughts', toughtsRoutes)
+app.get('/', ToughtController.showToughts)
+
 conn
-    //.sync({force : true}) Dados Relacionados
+    //.sync({force : true}) a tabela existente será excluída e criada novamente com base no modelo atualizado.
     .sync()
     .then(() => {
         app.listen(3000)
